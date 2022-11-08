5c2074b180d18f6dbd86bf691fe2afd9204b3461
Jessica Biagioni <100663378+JessicaBiagioni@users.noreply.github.com>
2022-11-08 10:18:38 -0300

    deploy1

--- PI-Videogames-main/client/src/index.js
+++ PI-Videogames-main/client/src/index.js
@@ -8,7 +8,7 @@ import {store} from "./store";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001/";
+axios.defaults.baseURL = "https://videogames-production.up.railway.app" || "http://localhost:3001";
 ReactDOM.render(
   <Provider store={store}>
   <React.StrictMode>
